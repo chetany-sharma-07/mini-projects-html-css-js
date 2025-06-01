cfee50957196f6b2d9fa7304e25bceb22b8775a2
chetany-sharma-07 <sharmachetany1004@gmail.com>
2025-06-01 16:30:37 +0530

    Adding date input and modify script.js to display date

--- 2_todo_app/script.js
+++ 2_todo_app/script.js
@@ -2,9 +2,12 @@ let todoList=[];
 
 function addTodo(){
     let inputElement=document.querySelector("#input-text");
+    let dateElement=document.querySelector("#input-date");
     let inputItem=inputElement.value;
-    todoList.push(inputItem)
+    let inputDate=dateElement.value;
+    todoList.push({item:inputItem,date:inputDate});
     inputElement.value="";
+    dateElement.value="";
     displayTodo();
 }
 
@@ -14,7 +17,8 @@ function displayTodo(){
     for(let i=0;i<todoList.length;i++){
         newHtml+=`
             <div>
-            <span>${todoList[i]}</span>
+            <span>${todoList[i].item}</span>
+            <span>${todoList[i].date}</span>
             <button onclick="todoList.splice(${i},1); displayTodo();">Delete</button>
             </div>
             `  
